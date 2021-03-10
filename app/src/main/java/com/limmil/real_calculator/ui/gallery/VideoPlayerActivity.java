f460abfcf658b25cb5055380ba8003f5f193c391
limmil <leecosecant@gmail.com>
2021-03-10 15:36:05 -0800

    small ui change

--- app/src/main/java/com/limmil/real_calculator/ui/gallery/VideoPlayerActivity.java
+++ app/src/main/java/com/limmil/real_calculator/ui/gallery/VideoPlayerActivity.java
@@ -57,6 +57,17 @@ public class VideoPlayerActivity extends AppCompatActivity {
         // prevents Android taking a screenshot when app goes to the background
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE,
                 WindowManager.LayoutParams.FLAG_SECURE);
+
+        // full screen mode
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
+
         // if file not found
         sourceVideoFile = new File(getIntent().getStringExtra("sourceVideoPath"));
         if (!sourceVideoFile.exists()){finish();}
